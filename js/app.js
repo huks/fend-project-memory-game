1bc6a929d94faa1889e7c93c697ca72e6e011c2b
huks <huks7417@gmail.com>
2017-11-22 10:21:33 +0900

    feat: deck event listener

--- js/app.js
+++ js/app.js
@@ -36,3 +36,12 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ // Add the functionality to handle clicks
+ const deck = document.getElementById("deck-id");
+ 
+ deck.addEventListener("click", function(event) {
+     if (event.target.nodeName === "LI") {
+         console.log(event.target.nodeName);
+     }
+ });
